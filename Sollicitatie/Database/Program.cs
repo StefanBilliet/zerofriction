a9ba911468b470f5f8d96491a9b61745db22fb22
Stefan Billiet <stefan.billiet@behindthebuttons.com>
2020-05-26 18:46:17 +0200

    forego zeit/cosmosdb-server since the current version doesn't work with C# and use the official emulator under Windows 🙄 add Database console application to set up the database and containers

---
+++ Sollicitatie/Database/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+
+namespace Database {
+  public static class Program {
+    public static async Task Main(string[] args) {
+      var client = new CosmosClientBuilder("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
+        .WithSerializerOptions(new CosmosSerializationOptions {PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase})
+        .Build();
+
+      await client.CreateDatabaseIfNotExistsAsync("zerofriction");
+      var database = client.GetDatabase("zerofriction");
+      await database.CreateContainerIfNotExistsAsync("customers", "/tenantId");
+    }
+  }
+}
