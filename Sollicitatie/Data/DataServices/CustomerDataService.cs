6866dc99bdec830d87218ab43350f48a29cdd6f8
Stefan Billiet <stefan.billiet@behindthebuttons.com>
2020-05-28 22:01:27 +0200

    finish add customer contact information

--- Sollicitatie/Data/DataServices/CustomerDataService.cs
+++ Sollicitatie/Data/DataServices/CustomerDataService.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Domain.State;
+using Infrastructure;
 
 namespace Data.DataServices {
   public interface ICustomerDataService {
-    Task<bool> CustomerExists(Guid id);
+    ValueTask<bool> CustomerExists(Guid id);
   }
 
   public class CustomerDataService : ICustomerDataService {
-    public Task<bool> CustomerExists(Guid id) {
-      throw new NotImplementedException();
+    private readonly IDbContext _dbContext;
+    private readonly ICurrentTenantProvider _currentTenantProvider;
+    private readonly ICosmosLinqQuery _cosmosLinqQuery;
+
+    public CustomerDataService(IDbContext dbContext, ICurrentTenantProvider currentTenantProvider, ICosmosLinqQuery cosmosLinqQuery) {
+      _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+      _currentTenantProvider = currentTenantProvider ?? throw new ArgumentNullException(nameof(currentTenantProvider));
+      _cosmosLinqQuery = cosmosLinqQuery ?? throw new ArgumentNullException(nameof(cosmosLinqQuery));
+    }
+
+    public ValueTask<bool> CustomerExists(Guid id) {
+      var query = _dbContext.Customers
+        .GetItemLinqQueryable<CustomerState>()
+        .Where(_ => _.TenantId == _currentTenantProvider.Get() && _.Id == id);
+
+      return _cosmosLinqQuery.GetFeedIterator(query).ToAsyncEnumerable().AnyAsync();
     }
   }
 }
---
+++ Sollicitatie/Tests/DataServices/CustomerDataServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Data;
+using Data.DataServices;
+using Domain.State;
+using FakeItEasy;
+using Infrastructure;
+using Tests.Infrastructure.CosmosDbTestingUtilities;
+using Xunit;
+
+namespace Tests.DataServices {
+  public class CustomerDataServiceTests {
+    private readonly IDbContext _dbContext;
+    private readonly CustomerDataService _sut;
+    private readonly ICurrentTenantProvider _currentTenantProvider;
+    private readonly Fixture _fixture;
+
+    public CustomerDataServiceTests() {
+      _dbContext = A.Fake<IDbContext>();
+      _currentTenantProvider = new CurrentTenantProvider();
+      _sut = new CustomerDataService(_dbContext, _currentTenantProvider, new DummyLinqQuery());
+      _fixture = new Fixture();
+    }
+
+    [Theory, AutoData]
+    public async Task GIVEN_no_customer_with_id_WHEN_CustomerExists_THEN_return_false(Guid customerId) {
+      A.CallTo(() => _dbContext.Customers).Returns(ContainerFactory.FakeContainer(new CustomerState[0]));
+
+      var customerExists = await _sut.CustomerExists(customerId);
+      
+      Assert.False(customerExists);
+    }
+    
+    [Fact]
+    public async Task GIVEN_customer_with_id_WHEN_CustomerExists_THEN_return_false() {
+      var expectedCustomerState = _fixture.Build<CustomerState>()
+        .With(_ => _.TenantId, _currentTenantProvider.Get())
+        .Create();
+      A.CallTo(() => _dbContext.Customers).Returns(ContainerFactory.FakeContainer(new []{expectedCustomerState}));
+
+      var customerExists = await _sut.CustomerExists(expectedCustomerState.Id);
+      
+      Assert.True(customerExists);
+    }
+  }
+}
